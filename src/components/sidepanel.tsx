a560a795671cd6a0043a74befc709f508775d199
Faadhil Hakim <faadhil@nexttrack.biz>
2022-08-10 23:09:20 -0500

    update sidepanel

--- src/components/sidepanel.tsx
+++ src/components/sidepanel.tsx
@@ -1,10 +1,6 @@
 import { useRecoilValue } from "recoil";
-import { Element, selectedElementType } from "stores/element.store";
 import { GraphicsPanel } from "./graphics-panel";
 import { PhotosPanel } from "./photos-panel";
-import { SelectedPhoto } from "./selected-photo";
-import { SelectedSvg } from "./selected-svg";
-import { SelectedText } from "./selected-text";
 import { SidebarItemState, sidebarState } from "./sidebar";
 import { TextPanel } from "./text-panel";
 
@@ -15,28 +11,12 @@ const sidepanelMap: Record<SidebarItemState, JSX.Element> = {
   text: <TextPanel />,
 };
 
-const selectedPanelMap: Record<Element["type"], JSX.Element> = {
-  rectangle: <div>Rectangle selected</div>,
-  image: <SelectedPhoto />,
-  svg: <SelectedSvg />,
-  text: <SelectedText />,
-  textBase: <SelectedText />,
-};
-
 export function Sidepanel() {
   const sidebarValue = useRecoilValue(sidebarState);
-  const selectedElement = useRecoilValue(selectedElementType);
 
   return (
     <>
       {sidepanelMap[sidebarValue]}
-      {selectedElement && (
-        <div
-          className="bg-base-200 absolute top-0 bottom-0 right-0 left-0"
-        >
-          {selectedPanelMap[selectedElement]}
-        </div>
-      )}
     </>
   );
 }
